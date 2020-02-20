891a94a27f7bebc2520cc05ce8e122ae18c833f0
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2020-02-19 21:34:03 -0800

    Update scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,28 +1,27 @@
 
 export class UserText {
   constructor (line1, line2, line3){
-    this.line1 = line1;
-    this.line2 = line2;
-    this.line3 = line3;
+    this.line1 = line1
+    this.line2 = line2
+    this.line3 = line3
   }
 
   findSyllables(line){
 
-    let spring = line.toLowerCase();
-    let array = spring.split(" "); 
-    console.log (array);
+    let spring = line.toLowerCase()
+    let array = spring.split(" ")
+    console.log (array)
    
-    let vowels = /[aeiouy]/i;
-    // var consonants = /[bcdfghjklmnpqrstvwxz]/i;
+    const vowels = /[aeiouy]/i
 
     let syllables = 0;
     
     for (let i=0; i<array.length; i++){
       for (let j=0; j<array[i].length; j++){
-        let currLetter = array[i].charAt(j);
-        let nextLetter = array[i].charAt(j+1);
-        let lastLetter = array[i].length-1;
-        let previousLetter = array[i].charAt(j-1);
+        let currLetter = array[i].charAt(j)
+        let nextLetter = array[i].charAt(j+1)
+        let lastLetter = array[i].length-1
+        let previousLetter = array[i].charAt(j-1)
         
         if (currLetter.match(vowels)){
           
@@ -30,22 +29,14 @@ export class UserText {
             continue
           } else {
             syllables++;
-            console.log (currLetter);
-            console.log (syllables);
+            console.log (currLetter)
+            console.log (syllables)
           }
         }
       }  
-      // } for (let x=0; x<array[i].length; x++) {
-      //   let consonantCheck = array[i].charAt(x);
-      //   let nextConsonant = array[i].charAt(x+1);
-      //   if (consonantCheck.match(consonants) && nextConsonant == "y") {
-      //     syllables ++
-      //     console.log(syllables);
-      //   } 
-      // }
     } return syllables
     
-  }
+   }
 
 }
 
